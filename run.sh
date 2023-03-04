44fff0857b9594c91a3eeea38145af165fb341a1
harshit-bansal18 <harshitb20@iitk.ac.in>
2023-03-04 18:42:04 +0530

    milestone1 done

--- run.sh
+++ run.sh
@@ -8,9 +8,16 @@ TEST="milestone1/tests/"
 make clean
 make milestone1
 
-for i in {1..10};
-do
-    echo "Running on test_$i.java"
-    ${MILE1}${BIN} --input ${TEST}"test_$i.java" --output build/milestone1/$i.gv
-    dot -Tpng build/milestone1/$i.gv > build/milestone1/$i.png
+for file in ${TEST}*.java
+do 
+    echo "Running on $file"
+    ${MILE1}${BIN} --input $file --output build/milestone1/$(basename $file .java).gv
+    dot -Tpng build/milestone1/$(basename $file .java).gv > build/milestone1/$(basename $file .java).png
 done
+
+# for i in {1..10};
+# do
+#     echo "Running on test_$i.java"
+#     ${MILE1}${BIN} --input ${TEST}"test_$i.java" --output build/milestone1/$i.gv
+#     dot -Tpng build/milestone1/$i.gv > build/milestone1/$i.png
+# done
